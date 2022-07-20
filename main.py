90e2fef7d8d4d31dd9507f81c7f601d4de80b375
ochi <ocos54ce@gmail.com>
2022-07-20 23:39:35 +0900

    csv

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
-# This is a sample Python script.
+import os
+import csv
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class Flow:
+    def __init__(self):
+        self.name = None
+        self.restaurant = None
 
+    def greet(self):
+        print('こんにちは!私はRobokoです。あなたの名前は何ですか?')
+        self.name = input()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    def question(self):
+        print('{}さん。どこのレストランが好きですか?'.format(self.name))
+        self.restaurant = input()
 
-# Press the green button in the gutter to run the script.
+    def bye(self):
+        print('{}さん。ありがとうございました。\n良い1日を！さようなら。'.format(self.name))
+
+    def count(self):
+        fileName = 'count.csv'
+        with open(fileName, 'w+') as csv_file:
+            fieldnames = ['Name', 'Count']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            dict = {}
+            with open(fileName, 'r') as read_csv:
+                reader = csv.DictReader(read_csv)
+                for row in reader:
+                    dict[row['Name']] = row['Count']
+
+            if self.restaurant in dict:
+                dict[self.restaurant] = dict[self.restaurant] + 1
+            else:
+                dict[self.restaurant] = 1
+
+            for key in dict.keys():
+                writer.writerow({'Name': key, 'Count': dict[key]})
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    f = Flow()
+    f.greet()
+    f.question()
+    f.count()
+    f.bye()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
